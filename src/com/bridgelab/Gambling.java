f3bae4957fda6f121e2d071d77ac161ae831e213
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-11-12 20:35:39 +0530

    UC1 - Stake of 00 every day and bet  every game

--- src/com/bridgelab/Gambling.java
+++ src/com/bridgelab/Gambling.java
@@ -1,7 +1,11 @@
 package com.bridgelab;
-
+//UC1 - As a Gambler, would start with a stake of $100 every day and bet $1 every game.
 public class Gambling {
     public static void main(String[] args) {
         System.out.println("Welcome to the Gambling Simulation Problem");
+        int stake = 100;
+        int bet = 1;
+
+        System.out.println("Today Stake :: " +stake+ " bet on each game is :: "+bet);
     }
 }
