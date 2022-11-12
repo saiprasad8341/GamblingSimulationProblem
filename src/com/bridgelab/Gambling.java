23ad9d9655400038d9b0b76711bd186186afd1f2
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-11-12 21:17:38 +0530

    UC4 - After 20 days of playing every day would like to know the total amount won or lost.

--- src/com/bridgelab/Gambling.java
+++ src/com/bridgelab/Gambling.java
@@ -1,19 +1,23 @@
 package com.bridgelab;
-//UC3 - As a Calculative Gambler if won or lost 50% of the stake, would resign for the day
+//UC4 - After 20 days of playing every day would like to know the total amount won or lost.
 public class Gambling {
     public static void main(String[] args) {
         System.out.println("Welcome to the Gambling Simulation Problem");
         int stake = 100;
-//        int bet = 1;
+        int wonAmount = 0;
 
-        while (stake > 50 && stake < 150) {
-            if (Math.random() < 0.5) {
-                stake++;
-                System.out.println("You won the game total stake is ::" + stake);
-            } else {
-                stake--;
-                System.out.println("You loose the game total stake is ::" + stake);
+        for (int i = 0; i < 20; i++) {
+            while (stake > 50 && stake < 150) {
+                if (Math.random() < 0.5) {
+                    stake++;
+                    System.out.println("You won the game total stake is ::" + stake);
+                } else {
+                    stake--;
+                    System.out.println("You loose the game total stake is ::" + stake);
+                }
             }
+            wonAmount += stake;
         }
+        System.out.println("Total amount won by after 20 days :: "+wonAmount);
     }
 }
