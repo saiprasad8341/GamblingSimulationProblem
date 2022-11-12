af0a9cc74c4ad29fc959585853b4ed4c0f34de00
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-11-12 20:49:08 +0530

    if won or lost 50% of the stake, would resign for the day

--- src/com/bridgelab/Gambling.java
+++ src/com/bridgelab/Gambling.java
@@ -1,17 +1,19 @@
 package com.bridgelab;
-//UC2 - As a Gambler make $1 bet so either win or loose $1
+//UC3 - As a Calculative Gambler if won or lost 50% of the stake, would resign for the day
 public class Gambling {
     public static void main(String[] args) {
         System.out.println("Welcome to the Gambling Simulation Problem");
         int stake = 100;
-        int bet = 1;
+//        int bet = 1;
 
-        if(Math.random() < 0.5){
-            stake++;
-            System.out.println("You won the game total stake is ::" + stake);
-        }else {
-            stake--;
-            System.out.println("You loose the game total stake is ::" + stake);
+        while (stake > 50 && stake < 150) {
+            if (Math.random() < 0.5) {
+                stake++;
+                System.out.println("You won the game total stake is ::" + stake);
+            } else {
+                stake--;
+                System.out.println("You loose the game total stake is ::" + stake);
+            }
         }
     }
 }
